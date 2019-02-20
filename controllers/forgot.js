2c3c05b22e235a26ab1ab7db5069db1052c140d4
mumo-dev <samumo.dev@gmail.com>
2019-02-20 08:14:15 +0300

    bug fixes 102

--- controllers/forgot.js
+++ controllers/forgot.js
@@ -1,261 +1,280 @@
-const User = require('../sequelize').User;
-const Admin = require('../sequelize').Admin;
-const PasswordResets = require('../sequelize').PasswordReset;
+const User = require("../sequelize").User;
+const Admin = require("../sequelize").Admin;
+const PasswordResets = require("../sequelize").PasswordReset;
 
-const bcrypt = require('bcrypt-nodejs');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
 module.exports = {
+  showforgetPassowrdView(req, res) {
+    res.render("forgot-password", {
+      title: "Forgot Password",
+      header: "show"
+    });
+  },
 
-    showforgetPassowrdView(req, res) {
-
-        res.render('forgot-password', {
-            title: 'Forgot Password',
-            header: 'show'
-        })
-    },
-
-    forgetPassword(req, res) {
-
-        Admin.findOne({
-            where: {
-                email: req.body.email
+  forgetPassword(req, res) {
+    Admin.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then(admin => {
+        if (admin) {
+          const email = admin.email;
+          const token = jwt.sign(
+            {
+              email: email,
+              id: admin.id
+            },
+            "secret",
+            {
+              expiresIn: "8760h" //after  a year
             }
-        }).then(admin => {
-            if (admin) {
-                const email = admin.email;
-                const token = jwt.sign(
-                    {
-                        email: email,
-                        id: admin.id
-                    },
-                    'secret',
-                    {
-                        expiresIn: '8760h' //after  a year
-                    }
-                );
+          );
 
-                //save the token in db;;
-                PasswordResets.create({
-                    token: token,
-                    email: email
-                }).then(resets => {
-                    //send email
-                    
-                    var smtpTransport = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: 'samumo.dev@gmail.com',
-                            pass: '33369324'
-                        }
-                    });
-                    var mailOptions = {
-                        to: email,
-                        from: 'samumo.dev@gmail.com',
-                        subject: 'Password Reset',
-                        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-                            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                            'https://' + req.headers.host + '/reset/' + token + '\n\n' +
-                            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-                    };
-                    smtpTransport.sendMail(mailOptions, function(err) {
-                        if(err) {
-                            res.status(500).json({
-                                message: 'Sending of email failed'
-                            })
-                        }
-                    });
-                    
+          //save the token in db;;
+          PasswordResets.create({
+            token: token,
+            email: email
+          }).then(resets => {
+            //send email
 
-                    return res.json({
-                        message: 'Password reset link has been send to your email with further instructions'
-                    })
-
-                })
-
-            } else {
-                return res.json({
-                    message: 'No account exists uses that email'
-                })
-            }
-        }).catch(err => {
-            return res.status(500).json({
-                error: err
-            })
-        })
-
-    },
-
-    showResetPasswordView(req, res) {
-        const token = req.params.token;
-        const id = req.query.id;
-
-        res.render('reset-password', {token, id, header:"show", title: "Reset Password"})
-
-    }
-    ,
-
-    resetPassword(req, res) {
-
-        const id = req.query.id;
-        const token = req.body.token;
-        const password = req.body.password;
-
-        const generateHash = function (password) {
-
-            return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-
-        };
-
-        PasswordResets.findOne({
-            where: {token: token}
-        }).then((result) => {
-            if (result) {
-                const email = result.email;
-                if(id == 2){
-                    User.findOne({
-                        where: {email: email}
-                    }).then((user) => {
-                        if (user) {
-                            const userPassword = generateHash(password);
-                            user.update({password: userPassword}, {where: {id: user.id}})
-                                .then(() => {
-                                    return res.json({
-                                        message: 'Password reset successful. Login in with your new password'
-                                    })
-                                }).catch(err => {
-
-                                res.status(500).json({
-                                    error: err
-                                })
-                            })
-                        } else {
-                            return res.json({
-                                message: 'Password reset failed. Try resending another password reset link'
-                            })
-                        }
-                    }).catch(err => {
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
-                }else {
-                    Admin.findOne({
-                        where: {email: email}
-                    }).then((admin) => {
-                        if (admin) {
-                            const userPassword = generateHash(password);
-                            admin.update({password: userPassword}, {where: {id: admin.id}})
-                                .then(() => {
-                                    return res.json({
-                                        message: 'Password reset successful. Login in with your new password',
-                                        redirect:true
-                                    })
-                                })
-                                .catch(err => {
-
-                                res.status(500).json({
-                                    error: err
-                                })
-                            })
-                        } else {
-                            return res.json({
-                                message: 'Password reset failed. Try resending another password reset link'
-                            })
-                        }
-                    }).catch(err => {
-                        res.status(500).json({
-                            error: err
-                        })
-                    })
-                }
-
-
-            } else {
-                return res.json({
-                    message: 'Password reset failed. Try resending another password reset link'
-                })
-            }
-        }).catch(err => {
-            res.status(500).json({
-                error: err
-            })
-        })
-    },
-
-    forgetUserPassword(req, res) {
-
-        User.findOne({
-            where: {
-                email: req.body.email
-            }
-        }).then(user => {
-            if (user) {
-                const email = user.email;
-                const token = jwt.sign(
-                    {
-                        email: email,
-                        id: user.id
-                    },
-                    'secret',
-                    {
-                        expiresIn: '8760h' //after  a year
-                    }
-                );
-
-                //save the token in db;;
-                PasswordResets.create({
-                    token: token,
-                    email: email
-                }).then(resets => {
-                    //send email
+            var smtpTransport = nodemailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: "samumo.dev@gmail.com",
+                pass: "33369324"
+              }
+            });
+            var mailOptions = {
+              to: email,
+              from: "samumo.dev@gmail.com",
+              subject: "Password Reset",
+              text:
+                "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+                "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+                "https://" +
+                req.headers.host +
+                "/reset/" +
+                token +
+                "\n\n" +
+                "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+            };
+            smtpTransport.sendMail(mailOptions, function(err, info) {
+              if (err) {
+                res.status(500).json({
+                  message: "Sending of email failed"
+                });
+              }else {
+                    console.log(info);
+                  }
               
-                    var smtpTransport = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: 'samumo.dev@gmail.com',
-                            pass: '33369324'
-                        }
-                    });
-                    const resetUrl = 'https://' + req.headers.host + '/reset/' + token +'?id=2';
-                    var mailOptions = {
-                        to: email,
-                        from: 'samumo.dev@gmail.com',
-                        subject: 'Password Reset',
-                        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-                            'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-                            resetUrl + '\n\n' +
-                            'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-                    };
-                    smtpTransport.sendMail(mailOptions, function(err) {
-                        // req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-                        if(err){
-                            console.log(err);
-                            return res.status(500).json({
-                                message: 'Failed to send email.'
-                            })   
-                        }
-                    });
-               
+            });
 
-                    return res.status(200).json({
-                        message: 'Password reset link has been send to your email with further instructions'
-                    })
+            return res.json({
+              message:
+                "Password reset link has been send to your email with further instructions"
+            });
+          });
+        } else {
+          return res.json({
+            message: "No account exists uses that email"
+          });
+        }
+      })
+      .catch(err => {
+        return res.status(500).json({
+          error: err
+        });
+      });
+  },
 
-                })
+  showResetPasswordView(req, res) {
+    const token = req.params.token;
+    const id = req.query.id;
 
-            } else {
-                return res.status(200).json({
-                    message: 'No account exists uses that email'
-                })
-            }
-        }).catch(err => {
-            return res.status(500).json({
-                error: err
+    res.render("reset-password", {
+      token,
+      id,
+      header: "show",
+      title: "Reset Password"
+    });
+  },
+  resetPassword(req, res) {
+    const id = req.query.id;
+    const token = req.body.token;
+    const password = req.body.password;
+
+    const generateHash = function(password) {
+      return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    };
+
+    PasswordResets.findOne({
+      where: { token: token }
+    })
+      .then(result => {
+        if (result) {
+          const email = result.email;
+          if (id == 2) {
+            User.findOne({
+              where: { email: email }
             })
-        })
+              .then(user => {
+                if (user) {
+                  const userPassword = generateHash(password);
+                  user
+                    .update(
+                      { password: userPassword },
+                      { where: { id: user.id } }
+                    )
+                    .then(() => {
+                      return res.json({
+                        message:
+                          "Password reset successful. Login in with your new password"
+                      });
+                    })
+                    .catch(err => {
+                      res.status(500).json({
+                        error: err
+                      });
+                    });
+                } else {
+                  return res.json({
+                    message:
+                      "Password reset failed. Try resending another password reset link"
+                  });
+                }
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                });
+              });
+          } else {
+            Admin.findOne({
+              where: { email: email }
+            })
+              .then(admin => {
+                if (admin) {
+                  const userPassword = generateHash(password);
+                  admin
+                    .update(
+                      { password: userPassword },
+                      { where: { id: admin.id } }
+                    )
+                    .then(() => {
+                      return res.json({
+                        message:
+                          "Password reset successful. Login in with your new password",
+                        redirect: true
+                      });
+                    })
+                    .catch(err => {
+                      res.status(500).json({
+                        error: err
+                      });
+                    });
+                } else {
+                  return res.json({
+                    message:
+                      "Password reset failed. Try resending another password reset link"
+                  });
+                }
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        } else {
+          return res.json({
+            message:
+              "Password reset failed. Try resending another password reset link"
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+  },
 
-    },
-}
-;
+  forgetUserPassword(req, res) {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then(user => {
+        if (user) {
+          const email = user.email;
+          const token = jwt.sign(
+            {
+              email: email,
+              id: user.id
+            },
+            "secret",
+            {
+              expiresIn: "8760h" //after  a year
+            }
+          );
+
+          //save the token in db;;
+          PasswordResets.create({
+            token: token,
+            email: email
+          }).then(resets => {
+            //send email
+
+            var smtpTransport = nodemailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: "samumo.dev@gmail.com",
+                pass: "33369324"
+              }
+            });
+            const resetUrl =
+              "https://" + req.headers.host + "/reset/" + token + "?id=2";
+            var mailOptions = {
+              to: email,
+              from: "samumo.dev@gmail.com",
+              subject: "Password Reset",
+              text:
+                "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+                "Please click on the following link, or paste this into your browser to complete the process:\n\n" +
+                resetUrl +
+                "\n\n" +
+                "If you did not request this, please ignore this email and your password will remain unchanged.\n"
+            };
+            smtpTransport.sendMail(mailOptions, function(err, info) {
+              // req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+              if (err) {
+                console.log(err);
+                return res.status(500).json({
+                  message: "Failed to send email."
+                });
+              } 
+            });
+
+            return res.status(200).json({
+              message:
+                "Password reset link has been send to your email with further instructions"
+            });
+          });
+        } else {
+          return res.status(404).json({
+            message: "No account exists uses that email"
+          });
+        }
+      })
+      .catch(err => {
+        return res.status(500).json({
+          error: err
+        });
+      });
+  }
+};
